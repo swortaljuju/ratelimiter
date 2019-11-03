47e8b0684405d26c88353b7ec43805a6717e92c9
swortal <swortal@gmail.com>
2019-11-02 17:35:18 -0700

    Refactor variable names and code a little bit.

--- ratelimiter/ratelimiter_middleware.py
+++ ratelimiter/ratelimiter_middleware.py
@@ -10,10 +10,6 @@ import math
 
 DUMMY_RATELIMITER_THRESHOLD = 7
 
-# Use constant number of tokens per bucket/window to avoid flushing in
-# short period.
-TOKEN_PER_BUCKET = 10
-
 # Redis client is thread safe because each connection is obtained only
 # when executing command.
 redis_client = redis.Redis(
@@ -22,61 +18,68 @@ redis_client = redis.Redis(
         'REDIS_PORT',
         cast=int))
 TOKEN_BUCKET_LUA = '''
-  local tokensPerBucket = %d; --[ Assign a small constant number of tokens always--]
-  local timeBucket = 10 / ARGV[1];
-  redis.call("SET", KEYS[1], tokensPerBucket, "EX", timeBucket, "NX");
-  local decrResult = redis.call("DECR", KEYS[1]);
+  local key = "token_bucket";
+  local tokensPerBucket = %d;
+  local timeBucket = 1;
+  redis.call("SET", key, tokensPerBucket, "EX", timeBucket, "NX");
+  local decrResult = redis.call("DECR", key);
   if (decrResult >= 0)
   then
     return 1;
   else
-    local pttlResult = redis.call("PTTL", KEYS[1]);
+    local pttlResult = redis.call("PTTL", key);
     if (pttlResult == -1)
     then
-       redis.call("SET", KEYS[1], (tokensPerBucket - 1), "EX", timeBucket);
+       redis.call("SET", key, (tokensPerBucket - 1), "EX", timeBucket);
        return 1;
     else
        return 0;
     end
   end
-''' % TOKEN_PER_BUCKET
+''' % RATE_THRESHOLD
 
 token_bucket_script = redis_client.register_script(TOKEN_BUCKET_LUA)
 
 LEAKY_BUCKET_LUA = '''
-  local tokensPerBucket = %d; --[ Assign a small constant number of tokens always--]
-  local timeBucket = 10 / ARGV[1];
-  redis.call("SET", KEYS[1], 0, "EX", timeBucket, "NX");
-  local incrResult = redis.call("INCR", KEYS[1]);
+  local key = "leaky_bucket";
+  local tokensPerBucket = %d;
+  local timeBucket = 1;
+  redis.call("SET", key, 0, "EX", timeBucket, "NX");
+  local incrResult = redis.call("INCR", key);
   if (incrResult <= tokensPerBucket)
   then
     if (incrResult == 1)
     then
-       local pttlResult = redis.call("PTTL", KEYS[1]);
+       local pttlResult = redis.call("PTTL", key);
        if (pttlResult == -1)
        then
-          redis.call("EXPIRE", KEYS[1], timeBucket);
+          redis.call("EXPIRE", key, timeBucket);
        end
     end
     return 1;
   else
     return 0;
   end
-''' % TOKEN_PER_BUCKET
+''' % RATE_THRESHOLD
 
 leaky_bucket_script = redis_client.register_script(LEAKY_BUCKET_LUA)
+
+# Use constant number of tokens per bucket/window to avoid flushing in
+# short period.
+TOKEN_PER_BUCKET = 10
 
 TIME_SEC_PER_BUCKET = TOKEN_PER_BUCKET / RATE_THRESHOLD
 
 SLIDING_WINDOW_LOG_LUA = '''
-  redis.call("ZREMRANGEBYSCORE", KEYS[1], -1/0, ARGV[1] - %f);
-  local windowSize = redis.call("ZCARD", KEYS[1]);
+  local key = "sliding_window_log";
+  redis.call("ZREMRANGEBYSCORE", key, -1/0, ARGV[1] - %f);
+  local windowSize = redis.call("ZCARD", key);
   if (windowSize == %d)
   then
     return 0;
   end
-  local value = redis.call("INCR", KEYS[1] .. "_counter");
-  redis.call("ZADD", KEYS[1], ARGV[1], value);
+  local value = redis.call("INCR", key .. "_counter");
+  redis.call("ZADD", key, ARGV[1], value);
   return 1;
 ''' % (TIME_SEC_PER_BUCKET, TOKEN_PER_BUCKET)
 
@@ -84,8 +87,9 @@ sliding_window_log_script = redis_client.register_script(
     SLIDING_WINDOW_LOG_LUA)
 
 SLIDING_WINDOW_PRORATE_LUA = '''
-  local currentKey = KEYS[1] .. KEYS[2];
-  local previousKey = KEYS[1] .. (KEYS[2] - 1);
+  local key = "sliding_window_prorate_"
+  local currentKey = key .. KEYS[1];
+  local previousKey = key .. (KEYS[1] - 1);
   redis.call("SET", currentKey, 0, "NX", "EX", math.max(1 , %d));
   local currentCnt = redis.call("GET", currentKey);
   local previousCnt = redis.call("GET", previousKey);
@@ -113,54 +117,52 @@ class RateLimiterMiddleware(MiddlewareMixin):
             print('ratelimiter middleware. threshold=%s' % RATE_THRESHOLD)
         # A ratelimiter to test manual_test_scripts.
         if '/dummy/' in request.path:
-            return self.__dummyLimit()
+            return self.__dummy_limit()
         elif '/token/' in request.path:
-            return self.__tokenLimit()
+            return self.__token_limit()
         elif '/leaky_token/' in request.path:
-            return self.__leakyTokenLimit()
+            return self.__leaky_token_limit()
         elif '/fixed_window/' in request.path:
-            return self.__fixedWindowLimit()
+            return self.__fixed_window_limit()
         elif '/sliding_window_log/' in request.path:
-            return self.__slidingWindowLogLimit()
+            return self.__sliding_window_log_limit()
         elif '/sliding_window_prorate/' in request.path:
-            return self.__slidingWindowProrateLimit()
+            return self.__sliding_window_prorate_limit()
         return None
 
-    def __dummyLimit(self):
+    def __dummy_limit(self):
         if (random.randrange(10) >= DUMMY_RATELIMITER_THRESHOLD):
             return self.__success()
         else:
             return self.__fail()
 
-    def __tokenLimit(self):
-        lua_result = token_bucket_script(
-            keys=["token_bucket"], args=[RATE_THRESHOLD])
-        return self.__parseLuaResult(lua_result)
+    def __token_limit(self):
+        lua_result = token_bucket_script()
+        return self.__parse_lua_result(lua_result)
 
-    def __leakyTokenLimit(self):
-        lua_result = leaky_bucket_script(
-            keys=["leaky_bucket"], args=[RATE_THRESHOLD])
-        return self.__parseLuaResult(lua_result)
+    def __leaky_token_limit(self):
+        lua_result = leaky_bucket_script()
+        return self.__parse_lua_result(lua_result)
 
-    def __fixedWindowLimit(self):
-        key = "%d_fixed_window" % self.__getCurrentWindow()
+    def __fixed_window_limit(self):
+        key = "%d_fixed_window" % self.__get_current_window()
         pipe = redis_client.pipeline()
         res = pipe.set(key, TOKEN_PER_BUCKET, ex=max(
             1, int(2 * TIME_SEC_PER_BUCKET)), nx=True).decr(key).execute()
         return self.__success() if res[1] >= 0 else self.__fail()
 
-    def __slidingWindowLogLimit(self):
-        lua_result = sliding_window_log_script(
-            keys=["sliding_window_log"], args=[time.time()])
-        return self.__parseLuaResult(lua_result)
+    def __sliding_window_log_limit(self):
+        lua_result = sliding_window_log_script(args=[time.time()])
+        return self.__parse_lua_result(lua_result)
 
-    def __slidingWindowProrateLimit(self):
-        currentTime = time.time()
-        currentWindow = self.__getFixedWindow(currentTime)
-        previousWindowPortion = currentWindow + 1 - currentTime / TIME_SEC_PER_BUCKET
+    def __sliding_window_prorate_limit(self):
+        current_time = time.time()
+        current_window = self.__get_fixed_window(current_time)
+        previous_window_portion = current_window + \
+            1 - current_time / TIME_SEC_PER_BUCKET
         lua_result = sliding_window_prorate_script(
-            keys=["sliding_window_prorate_", currentWindow], args=[previousWindowPortion])
-        return self.__parseLuaResult(lua_result)
+            keys=[current_window], args=[previous_window_portion])
+        return self.__parse_lua_result(lua_result)
 
     def __fail(self):
         return HttpResponse(status=HTTPStatus.TOO_MANY_REQUESTS)
@@ -168,11 +170,11 @@ class RateLimiterMiddleware(MiddlewareMixin):
     def __success(self):
         return None
 
-    def __parseLuaResult(self, lua_result):
+    def __parse_lua_result(self, lua_result):
         return (self.__success() if lua_result == 1 else self.__fail())
 
-    def __getCurrentWindow(self):
-        return self.__getFixedWindow(time.time())
+    def __get_current_window(self):
+        return self.__get_fixed_window(time.time())
 
-    def __getFixedWindow(self, time_sec):
+    def __get_fixed_window(self, time_sec):
         return int(math.floor(time_sec / TIME_SEC_PER_BUCKET))
